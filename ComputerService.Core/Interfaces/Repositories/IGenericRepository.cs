6d51924033c89572f9720520b6e5db554e7cf941
tamoxis <tamoxis@gmail.com>
2021-02-01 16:45:49 +0100

    Refactoring code

--- ComputerService.Core/Interfaces/Repositories/IGenericRepository.cs
+++ ComputerService.Core/Interfaces/Repositories/IGenericRepository.cs
@@ -1,5 +1,4 @@
-﻿using ComputerService.Common.Helpers;
-using Microsoft.EntityFrameworkCore.Query;
+﻿using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,70 +25,7 @@ namespace ComputerService.Core.Interfaces.Repositories
             Func<IQueryable<TResult>, IOrderedQueryable<TResult>> orderBy = null,
             int? take = null);
 
-        Task UpdateManyAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool detachAll = false);
-
         Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
-
-        Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
-
-        Task<TEntity> GetFirstAsync(
-            Expression<Func<TEntity, bool>> predicate,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
-
-        Task<TResult> GetFirstAsync<TResult>(
-            Expression<Func<TEntity, bool>> predicate,
-            Expression<Func<TEntity, TResult>> select,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
-
-        Task<TEntity> GetFirstOrDefaultAsync(
-            Expression<Func<TEntity, bool>> predicate,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
-
-        Task<TResult> GetFirstOrDefaultAsync<TResult>(
-            Expression<Func<TEntity, bool>> predicate,
-            Expression<Func<TEntity, TResult>> select,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
-
-        Task<TEntity> GetSingleAsync(
-            Expression<Func<TEntity, bool>> predicate,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
-
-        Task<TResult> GetSingleAsync<TResult>(
-            Expression<Func<TEntity, bool>> predicate,
-            Expression<Func<TEntity, TResult>> select,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
-
-        Task<TEntity> GetSingleOrDefaultAsync(
-            Expression<Func<TEntity, bool>> predicate,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
-
-        Task<TResult> GetSingleOrDefaultAsync<TResult>(
-            Expression<Func<TEntity, bool>> predicate,
-            Expression<Func<TEntity, TResult>> select,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
-
-        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-            Func<TEntity, dynamic> distinctBy = null);
-
-        Task<IEnumerable<TEntity>> GetAllAsyncWithPredicate(CancellationToken cancellationToken,
-            Expression<Func<TEntity, bool>> predicate,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-            Func<TEntity, dynamic> distinctBy = null);
 
         Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken);
 
@@ -101,24 +37,5 @@ namespace ComputerService.Core.Interfaces.Repositories
 
         Task<TEntity> UpdateAsync(CancellationToken cancellationToken, TEntity entity,
             bool detachAll = false);
-
-        Task DeletePermanentlyAsync(TEntity entity, CancellationToken cancellationToken, bool detachAll = false);
-
-        Task DeletePermanentlyByIdAsync<TId>(TId id, CancellationToken cancellationToken, bool detachAll = false);
-
-        IEnumerable<TEntity> GetPage(IEnumerable<TEntity> list, byte pageSize = 16, int pageNumber = 0);
-
-        Task<int> GetNumberOfPagesAsync(CancellationToken cancellationToken, byte pageSize = 16,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-            Func<TEntity, dynamic> distinctBy = null);
-
-        Task<PaginatedList<TEntity>> GetAsync(
-            Expression<Func<TEntity, bool>> predicate,
-            int pageSize,
-            int pageNumber,
-            CancellationToken cancellationToken,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
     }
 }
